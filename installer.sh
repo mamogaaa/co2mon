f3e8e860a199b5bc2e28c205e70ae56b231d2b84
Your Name <you@example.com>
2023-10-11 16:20:48 +0000

    fixes

--- installer.sh
+++ installer.sh
@@ -28,4 +28,5 @@ sudo cp $SOURCE_DIR/start_co2mond /usr/local/bin/start_co2mond
 sudo cp $SOURCE_DIR/co2mond.service /etc/systemd/system/co2mond.service
 sudo chmod 664 /etc/systemd/system/co2mond.service
 sudo systemctl daemon-reload
+sudo systemctl enable co2mond
 sudo systemctl restart co2mond
